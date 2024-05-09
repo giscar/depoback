ecb7ea4cd9ff504825580dd4fd38675e5a7fbc17
= <careli_2710@hotmail.com>
2024-05-09 07:46:46 -0500

    avance

--- src/main/java/model/Servicio.java
+++ src/main/java/model/Servicio.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Document(collection = "servicio")
+@Document(collection = "servicios")
 public class Servicio {
 	
 	@Id
