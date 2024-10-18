0807af0cd184015338e608f1c1e3a5c75779889b
= <careli_2710@hotmail.com>
2024-10-18 11:18:24 -0500

    desarrollo factura electronica

---
+++ src/main/java/model/Perfil.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document(collection = "perfiles")
+public class Perfil implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	
+	private String codigo;
+	
+	private String descripcion;
+	
+	private String estado;
+	
+	private Date fechaRegistro = new Date();
+	
+	private String usuarioRegistro;
+	
+	private String indInactivo;
+
+}
